7c25b507e155259d2ca95212c11500c24fec50ff
tamik77777 <kosmantami@gmail.com>
2023-05-16 01:41:07 +0300

    url

--- src/service.js
+++ src/service.js
@@ -47,7 +47,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 //axios.defaults.baseURL = process.env.apiUrl;
 const apiClient=axios.create({
-  baseURL:process.env.REACT_APP_API_URL
+  baseURL:`${process.env.REACT_APP_API_URL}/items`
 })
 console.log('process.env.REACT_APP_API_URL', process.env.REACT_APP_API_URL)
 //axios.create();
